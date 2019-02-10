6c9deb44312f4d6e5eef4fafe931300b044db5cd
12ColcloughJ <joshuacolclough2@googlemail.com>
2019-02-10 20:13:56 +0000

    Spent way too long on not getting anywhere

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ const (
   SCREEN_W float64 = 1400
   SCREEN_H float64 = 800
 
-	maxIteration float64 = 10
+	maxIteration float64 = 100
 	offsetSpd float64 = 10
 	zoomSpd float64 = 0.2
 )
@@ -19,9 +19,9 @@ func main() {
 
 	rendered := false
 
-	var focusX, focusY float64 = 0, 0
-	w, h := SCREEN_W, SCREEN_H
-	var zoom float64 = 0.5
+	var focusX, focusY float64 = -SCREEN_W/2, -SCREEN_H/2
+	var w, h float64 = SCREEN_W, SCREEN_H
+	var zoom float64 = 1
 
   for !rl.WindowShouldClose() {
 		if rl.IsKeyDown(rl.KeyA) {   // go left
@@ -43,13 +43,19 @@ func main() {
 
 		mouseMv := float64(rl.GetMouseWheelMove())
 		if mouseMv != 0 {
-			zoom += zoomSpd*mouseMv
+			dz := zoomSpd*mouseMv*zoom
+			oldZoom := zoom
+			zoom += dz
+			w, h = w/zoom, h/zoom
+			focusX -= (w * (1 - 1/(zoom/oldZoom)))
+			focusY -= (h * (1 - 1/(zoom/oldZoom)))
+			println(focusX, focusY, dz, w, h, "focX, focY, dz, w, h")
 			rendered = false
 		}
 
 		if rl.IsMouseButtonDown(0) {
-			focusX += float64(rl.GetMouseX()) - w/2
-			focusY += float64(rl.GetMouseY()) - h/2
+			focusX += (float64(rl.GetMouseX()) - SCREEN_W/2)
+			focusY += (float64(rl.GetMouseY()) - SCREEN_H/2)
 			println(focusX, focusY)
 			rendered = false
 		}
@@ -58,7 +64,8 @@ func main() {
 
 		if !rendered {
 			rl.ClearBackground(rl.Black)
-			mandelbrotFull(focusX, focusY, w, h, zoom, maxIteration)
+			mandelbrotFull(focusX, focusY, SCREEN_W, SCREEN_H, zoom, maxIteration)
+			rl.DrawPixel(-int32(focusX), -int32(focusY), rl.Red)
 			rendered = true
 		}
 
