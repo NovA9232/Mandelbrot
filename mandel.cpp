e6be2acb9199569b4bf62f7e1ea6ee51074af191
12ColcloughJ <joshuacolclough2@googlemail.com>
2019-03-17 21:37:49 +0000

    Added mouse wheel zoom

--- mandel.cpp
+++ mandel.cpp
@@ -1,5 +1,7 @@
 #include "raylib.h"
 
+#define maxIterDefault 100
+
 int mandelbrotPixel(double i, double j, int W, int H, int maxIteration, double zoom) {
 	double zx = ((i/double(W))*2.5)/zoom;
 	double zy = ((j/double(H))*1.5)/zoom;
@@ -32,16 +34,24 @@ void fullMandelbrot(double offX, double offY, int W, int H, double zoom, int max
 int main(int argc, char* argv[]) {
 	int screenWidth = 1000;
 	int screenHeight = 600;
+	double const zoomSpd = 0.2;
 
 	InitWindow(screenWidth, screenHeight, "Mandelbrot");
 	SetTargetFPS(144);
 
 	double offX = -800;
 	double offY = -300;
+	double zoom = 0.6;
 
 	bool rendered = false;
 
 	while (!WindowShouldClose()) {
+		double mouseMv = double(GetMouseWheelMove());
+		if (mouseMv != 0) {
+			double dz = zoomSpd*mouseMv*zoom;
+			zoom += dz;
+			rendered = false;
+		}
 
 		if (IsMouseButtonDown(0)) {
 			offX += (double(GetMouseX()) - double(screenWidth/2));
@@ -52,7 +62,7 @@ int main(int argc, char* argv[]) {
 		BeginDrawing();
 		if (!rendered) {
 			ClearBackground(BLACK);
-			fullMandelbrot(offX, offY, screenWidth, screenHeight, 1, 100);
+			fullMandelbrot(offX, offY, screenWidth, screenHeight, zoom, maxIterDefault);
 			rendered = true;
 		}
 
